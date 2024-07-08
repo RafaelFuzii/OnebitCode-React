3941261548862af384605feb49a39b1f1e7e6f08
RafaelFuzii <yuzofuzii@gmail.com>
2024-07-08 17:08:41 -0300

    Aula Props

--- Exercicio2/react-exercise-2/src/App.jsx
+++ Exercicio2/react-exercise-2/src/App.jsx
@@ -1,12 +1,16 @@
 import Card from "./components"
-
+import posterImg from "./assets/start-wars.jpg"
+import pesbPosterImg from "./assets/esb-poster.jpg"
+import rotjPosterImg from "./assets/rotj-poster.jpg"
 
 function App() {
 
 
   return (
     <>
-      <Card />
+      <Card title="Pôster: Star Wars (1977)" posterImg={posterImg}/>
+      <Card title="Pôster: Empire Stikes Back (1980)" posterImg={pesbPosterImg}/>
+      <Card title="Pôster: Return of the Jedi (1983)" posterImg={rotjPosterImg}/>
     </>
   )
 }
--- Exercicio2/react-exercise-2/src/components/index.jsx
+++ Exercicio2/react-exercise-2/src/components/index.jsx
@@ -1,12 +1,11 @@
-import starWars from '/start-wars.jpg'
 import styles from "./styles.module.css"
 
-function ImageStar(){
+function Card({ title, posterImg }){
     return(
         <div className={styles.container}>
-            <img src={starWars} alt="" className={styles.imageStarWars}/>
+            <img src={posterImg} alt="" className={styles.imageStarWars}/>
             <div className={styles.content}>
-                <h2 className={styles.title}>Pôster: Star Wars (1977)</h2>
+                <h2 className={styles.title}>{title}</h2>
                 <p className={styles.paragraph}>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
                 <button>Comprar agora</button>
             </div>
@@ -14,4 +13,4 @@ function ImageStar(){
     )
 }
 
-export default ImageStar
+export default Card
